25b1059a9062034392f729dcd89ea14296e25e5c
TheBrendinator <b.scott2@rgu.ac.uk>
2024-10-28 10:36:05 +0000

    Lab 6 Async

--- lab_6/task3maybe/scripts/timer.js
+++ lab_6/task3maybe/scripts/timer.js
@@ -6,12 +6,15 @@
  * @Last modified time: 14-Feb-192019
  */
 
-setCountdown(10);
+setCountdown(10, boom);
+setCountdown(11, bigboom);
+setCountdown(12, biggerboom);
 
- function setCountdown(delay){
+ function setCountdown(delay,callback){
    //sets an timeout delay to display a message after the set delay
    setTimeout(function() {
-     console.log("boom")
+    boom();
+    callback();
    }, delay*1000);
 
    //reduces the seconds left by 1
@@ -20,9 +23,23 @@ setCountdown(10);
    var counter = setInterval(function() {
 
      //display the current seconds left
-     console.log(secondsleft--);
+     //console.log(secondsleft--);
+     document.getElementById('countdown').innerHTML = secondsleft--;
 
      //if we are at 0 stop the countdown
      if(secondsleft <=0){clearInterval(counter)}
    }, 1000);
+ }
+
+ function boom(){
+  document.getElementById('countdown').innerHTML = "boom";
+  document.getElementById('alarm').innerHTML = "<img id='alarmimage' src='alarm.jpg' />"
+ }
+
+ function bigboom(){
+  document.getElementById('alarm').innerHTML = "<img id='alarmimage' src='bam.jpg' />"
+ }
+
+ function biggerboom(){
+  document.getElementById('alarm').innerHTML = "<img id='alarmimage' src='BigBadaBoom.jpg' />"
  }
